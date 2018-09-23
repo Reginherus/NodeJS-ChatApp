671689556515927ef7d9f8331b63904e58eae6b9
Nathan Miller <Natemiller24516@gmail.com>
2018-09-22 22:04:26 -0700

    Auto scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom() {
+//Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+//Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
+
 socket.on('connect', function() {
   console.log("Connected to server.");
 });
@@ -16,7 +33,8 @@ socket.on('newMessage', function (message) {
     createdAt: moment(message.createdAt).format('h:mm a')
   });
 
-  jQuery('#message').append(html);
+  jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -27,7 +45,8 @@ socket.on('newLocationMessage', function (message) {
     url: message.url
   });
   
-  jQuery('#message').append(html);
+  jQuery('#messages').append(html);
+  scrollToBottom();
 })
 
 jQuery('#message-form').on('submit', function (e) {
